99ae2c213f1913d20680f4e926605e2cf88b0359
rlowman <robertlowman@kings.edu>
2017-12-11 23:36:27 -0500

    deleting polls

--- mysite/polls/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^projects', views.projects, name='projects'),
-    url(r'^contact', views.contact, name='contact'),
-]
