632e16e6b33407cb018c5db34331a7c59bda885d
rlowman <robertlowman@kings.edu>
2018-05-30 22:31:00 -0400

    adding favicon2

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ["SECRET_KEY_MYSITE"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['rlowman.pythonanywhere.com', 'localhost']
 
